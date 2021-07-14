78cf0034c8e4355284a77073c03896491bd3578f
Ivan Reed <ivanp.reed@gmail.com>
2021-07-13 21:45:14 -0500

    week4

---
+++ Week4/4.1.1-Evr-SelectingDOMElements.js
@@ -0,0 +1,8 @@
+// select all unordered list (ul) elements
+console.log(document.getElementsByTagName("ul"));
+// select all elements with the class "class-week"
+console.log(documents.getElementsByClassName("class-week"));
+// select all elements with the class "nav-bar"
+console.log(documents.getElementsByClassName("nav-bar"));
+// select the element with the id "dog-picture" and save it to a variable
+let var= document.getElementsById()
---
+++ Week4/4.1.3-Evr-ChangingElements.js
@@ -0,0 +1,16 @@
+const { doc } = require("prettier");
+
+// print the outer HTML of the navigation bar
+const navbar = document.querySelector()
+console.log("nav bar"), navBar.oue
+// print the inner HTML of the navigation bar
+
+// print the text content of the #class-schedule-list element
+
+// select the classList for the first class-week, then add the class "week-1"
+
+// select the img element and add a src attribute
+document.querySelector("img").src=
+"https://i2.pickpik.com/photos/774/556/1024/odin-male-australian-shepherd-dog-thumb.jpg";
+
+// change the font color of the h1 element
